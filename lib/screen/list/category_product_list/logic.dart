fd52b67bb5042eee7759d6a13451ae9d64314dad
diandian <hlxsmail@gmail.com>
2021-04-13 20:46:09 +0800

    add category products

--- lib/screen/list/category_product_list/logic.dart
+++ lib/screen/list/category_product_list/logic.dart
@@ -5,18 +5,24 @@ import 'package:dd_taoke_sdk/model/category.dart';
 import 'package:dd_taoke_sdk/model/product.dart';
 import 'package:dd_taoke_sdk/params/product_list_param.dart';
 import 'package:dd_tk_shop/functions/fun_verify.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class CategoryProductListLogic extends GetxController {
-  static CategoryProductListLogic get instance => Get.find<CategoryProductListLogic>();
+  static CategoryProductListLogic get instance =>
+      Get.find<CategoryProductListLogic>();
   var panentId = ''.obs; // 父分类id
   var childId = ''.obs; // 子分类id
 
   var pageId = 1.obs; // 第几页
-  var sort = 0.obs; // 排序
+  var sort = '0'.obs; // 排序
 
   var products = <Product>[].obs; // 显示的商品列表
+
+  ScrollController scrollController = ScrollController();
+  EasyRefreshController refreshController = EasyRefreshController();
 
   @override
   void onInit() {
@@ -36,6 +42,23 @@ class CategoryProductListLogic extends GetxController {
     update();
   }
 
+  /// 刷新
+  Future<void> fresh() async {
+    scrollController.jumpTo(0);
+    products.clear();
+    pageId.value = 1;
+    await loadData();
+  }
+
+  /// 用户需要排序
+  Future<void> onSort(String sortValue) async {
+    scrollController.jumpTo(0);
+    sort.value = sortValue;
+    products.clear();
+    pageId.value = 1;
+    await fresh();
+  }
+
   /// 组装参数
   ProductListParam get getParams => ProductListParam(
       pageId: '${pageId.value}',
@@ -44,18 +67,18 @@ class CategoryProductListLogic extends GetxController {
       cids: '${childId.value},${panentId.value}');
 
   /// 加载数据
-  Future<void> loadData() async {
-    print(jsonEncode(getParams));
+  Future<bool> loadData() async {
     final result = await DdTaokeSdk.instance.getProducts(param: getParams);
     if (!isNull(result)) {
       products.addAll(result.list);
       update();
     }
+    return result != null;
   }
 
   /// 加载下一页
   Future<void> nextPage() async {
-    pageId.value +=1;
+    pageId.value += 1;
     await loadData();
     update();
   }
--- lib/screen/list/category_product_list/view.dart
+++ lib/screen/list/category_product_list/view.dart
@@ -1,3 +1,4 @@
+import 'package:dd_taoke_sdk/constant/sort.dart';
 import 'package:dd_tk_shop/widget/product/wall_product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
@@ -14,10 +15,45 @@ class CategoryProductListPage extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: Text('分类'),
+            actions: [
+              PopupMenuButton<String>(
+                itemBuilder: (_) {
+                  return [
+                    PopupMenuItem(
+                      child: Text('综合排序'),
+                      value: '0',
+                    ),
+                    PopupMenuItem(
+                      child: Text('最高人气'),
+                      value: DdSort.couponHighToLow,
+                    ),
+                    PopupMenuItem(
+                      child: Text('销量从高到低'),
+                      value: DdSort.salesHighToLow,
+                    ),
+                    PopupMenuItem(
+                      child: Text('价格从低到高'),
+                      value: DdSort.priceLowToHigh,
+                    ),
+                    PopupMenuItem(
+                      child: Text('价格从高到低'),
+                      value: DdSort.priceHighToLow,
+                    )
+                  ];
+                },
+                icon: Icon(Icons.sort),
+                onSelected: logic.onSort,
+              )
+            ],
           ),
           body: EasyRefresh.custom(
+            scrollController: logic.scrollController,
+            controller: logic.refreshController,
             slivers: [_productList()],
             onLoad: logic.nextPage,
+            onRefresh: logic.fresh,
+            header: MaterialHeader(),
+            footer: MaterialFooter(),
           ),
         );
       },
