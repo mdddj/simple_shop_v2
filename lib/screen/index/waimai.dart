1bf5360474f0ba3894270958f86a43251bb9373b
mdddj <413153189@qq.com>
2022-03-26 17:54:42 +0800

    首页新增外卖券

--- lib/screen/index/waimai.dart
+++ lib/screen/index/waimai.dart
@@ -29,6 +29,11 @@ class _WaimaiState extends State<Waimai> {
     }
   }
 
+  //美团外卖券
+  Future<void> _meituan() async {
+
+  }
+
   Widget _renderItem(String svg,String title,String subTitle,VoidCallback onTap) {
     return Container(
       child: Row(
