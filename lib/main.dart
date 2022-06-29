c0eeac5a9e764626bc016accb3810a25c3f9dcda
mddd <413153189@qq.com>
2022-06-29 17:12:39 +0800

    升级依赖

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,8 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
